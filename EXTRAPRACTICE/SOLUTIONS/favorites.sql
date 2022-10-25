d1a3340deb9406cd36c9432d5749a34cc4db2936
Gabe LeBlanc <90121815+gblanc25@users.noreply.github.com>
2022-10-25 11:48:36 -0400

    Create favorites.sql

---
+++ EXTRAPRACTICE/SOLUTIONS/favorites.sql
@@ -0,0 +1,67 @@
+-- Adventure Time (LIKE is case-insensitive)
+UPDATE shows SET title = "Adventure Time" WHERE title LIKE "Adventure Time";
+
+-- Arrow
+UPDATE shows SET title = "Arrow" WHERE title LIKE "Arrow";
+
+-- Avatar: The Last Airbender
+UPDATE shows SET title = "Avatar: The Last Airbender" WHERE title LIKE "Avatar%";
+
+-- Billions
+UPDATE shows SET title = "Billions" WHERE title LIKE "Billions%";
+
+-- Brooklyn Nine-Nine
+UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title LIKE "Brooklyn%99";
+UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title LIKE "B99";
+
+-- Community
+UPDATE shows SET title = "Community" WHERE title LIKE "Community";
+
+-- Criminal Minds
+UPDATE shows SET title = "Criminal Minds" WHERE title LIKE "Criminal Minds";
+
+-- Family Guy
+UPDATE shows SET title = "Family Guy" WHERE title LIKE "Family Guy";
+
+-- Friends
+UPDATE shows SET title = "Friends" WHERE title LIKE "Friends";
+
+-- Game of Thrones
+UPDATE shows SET title = "Game of Thrones" WHERE title LIKE "Game of Thrones";
+UPDATE shows SET title = "Game of Thrones" WHERE title LIKE "GoT";
+
+-- Gilmore Girls
+UPDATE shows SET title = "Gilmore Girls" WHERE title LIKE "Gilmore Girls";
+
+-- Grey's Anatomy
+UPDATE shows SET title = "Grey's Anatomy" WHERE title LIKE "Grey's Anatomy%";
+
+-- How I Met Your Mother
+UPDATE shows SET title = "How I Met Your Mother" WHERE title LIKE "How I Met Your Mother";
+
+-- It's Always Sunny in Philadelphia
+UPDATE shows SET title = "It's Always Sunny in Philadelphia" WHERE title = "Its Always Sunny in Philidelphia";
+
+-- Parks and Recreation
+UPDATE shows SET title = "Parks and Recreation" WHERE title LIKE "Parks and Rec%";
+
+-- Sherlock
+UPDATE shows SET title = "Sherlock" WHERE title LIKE "Sherlock%";
+
+-- Squid Game
+UPDATE shows SET title = "Squid Game" WHERE title LIKE "Squid Game";
+
+-- The Bachelorette
+UPDATE shows SET title = "The Bachelorette" WHERE title LIKE "The Bachelorette";
+
+-- The Crown
+UPDATE shows SET title = "The Crown" WHERE title LIKE "The Crown";
+
+-- The Office
+UPDATE shows SET title = "The Office" WHERE title LIKE "%Office%";
+
+-- The Queen's Gambit
+UPDATE shows SET title = "The Queen's Gambit" WHERE title LIKE "Queen's Gambit";
+
+-- The Untamed
+UPDATE shows SET title = "The Untamed" WHERE title LIKE "The Untamed";
